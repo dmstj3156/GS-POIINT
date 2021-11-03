7b58ba38bc306aad9303c2f939e2e84fb65a48c0
dmstj3156 <84954658+dmstj3156@users.noreply.github.com>
2021-11-03 13:59:00 +0900

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -7,3 +7,27 @@ $(function(){
         $('.depth2, .nav_bg').removeClass('show')
       })
 });
+
+$(function(){
+  $('#toggle').click(function(){
+    $('nav').toggleClass('show');
+  })
+  if($(this).hasClass('active')){
+    //열림버튼 표시
+   
+  } else {
+    
+    //닫힘버튼 표시
+   
+  }
+})
+$(function(){
+  $('#toggle').on('click', function(){
+    $('#gnb,.depth1').show();
+});
+
+// 메인메뉴 닫기
+$('.btn_close').on('click', function(){
+    $('#gnb').hide();
+});
+})
